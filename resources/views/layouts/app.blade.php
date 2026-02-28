1316e42c4029934300754d4863aed73497891894
satyam <satyamsingh962572@gmail.com>
2026-02-28 16:02:57 +0530

    initally commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link rel="icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
 </head>
 
 <body class="bg-gray-50">
